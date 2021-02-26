10b0b850f3585357d4d5f23ac3264f4036212974
Sean <sean@vestaridefair.com>
2021-02-25 23:26:10 -0800

    Feb 25 11:26PM

--- pages/class_component/test.js
+++ pages/class_component/test.js
@@ -15,6 +15,9 @@ import {
   WrapItem,
   VStack,
   Input,
+  Grid,
+  GridItem,
+  Badge,
   Box,
 } from '@chakra-ui/react';
 
@@ -23,36 +26,74 @@ class List extends Component {
     return (
       <>
         <VStack bg="gray.200" h="100vh">
-          <Stack align="center" mt={20} w="50%" rounded="2xl" mb={20}>
+          <VStack mt={20} mb={20}>
+            <Badge
+              variant="outline"
+              colorScheme="blue"
+              mb={20}
+              fontSize="20px"
+              borderRadius="xl"
+              p={5}
+            >
+              #seanmodd
+            </Badge>
             {this.props.items.map((item) => (
               <Box
-                align="center"
                 borderRadius="xl"
-                bg="red.200"
-                w="500px"
-                align="center"
-                textAlign="center"
-                justifyContent="center"
-                h="60px"
+                bg="white"
+                h="40px"
+                w="350px"
                 shadow="md"
                 my={5}
                 key={item.id}
               >
-                <Heading mt={1.5}>{item}</Heading>
+                <Heading
+                  color="blue.400"
+                  mt={1.5}
+                  textAlign="center"
+                  fontSize="20px"
+                >
+                  {item}
+                </Heading>
               </Box>
             ))}
-          </Stack>
-          <Box bg="gray.200" h="auto" w="100vw" align="center">
-            <FormControl>
-              <Stack w="50%" align="center">
-                <Input shadow="md" bg="white" w="110" type="text" />
-                <Input bg="white" w="110" type="text" />
-                <Button shadow="md" h="50px" w="100px" type="submit">
-                  <Heading fontSize="20px">submit</Heading>
-                </Button>
-              </Stack>
-            </FormControl>
-          </Box>
+          </VStack>
+
+          <Grid h="20%" justify="center" w="100%" align="center">
+            <GridItem>
+              <Input
+                placeholder="put something here"
+                shadow="md"
+                bg="lightyellow"
+                w="110"
+                type="text"
+              />
+            </GridItem>
+            <GridItem>
+              <Input
+                placeholder="put another thing here"
+                bg="lightyellow"
+                shadow="md"
+                my={5}
+                w="110"
+                type="text"
+              />
+            </GridItem>
+            <GridItem>
+              <Button
+                bg="green.400"
+                shadow="md"
+                h="50px"
+                my={5}
+                w="100px"
+                type="submit"
+              >
+                <Heading color="white" fontSize="20px">
+                  SUBMIT!
+                </Heading>
+              </Button>
+            </GridItem>
+          </Grid>
         </VStack>
       </>
     );
@@ -61,7 +102,7 @@ class List extends Component {
 
 const ListComponent = () => (
   <div>
-    <List items={['one', 'two', 'THREEEEMOTHERFUCKERS']} />
+    <List items={['one', 'two', 'three']} />
   </div>
 );
 
